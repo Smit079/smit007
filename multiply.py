34d72d76bf22b0856b3b72116423a4bb5dbb3a2e
Smit079 <smitpatel0718@gmail.com>
2025-02-17 15:27:05 +0530

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,11 @@
+def multiply_numbers(a, b):
+    return a * b
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+result = multiply_numbers(num1, num2)
+
+print(f"The result of {num1} × {num2} is: {result}")
